4969367823f247ba737fa6eb1ff6d197a2047373
turulomio <turulomio@yahoo.es>
2026-04-26 20:26:32 +0200

    Fixed tests

--- eml_rename/tests/test_emlfile.py
+++ eml_rename/tests/test_emlfile.py
@@ -10,12 +10,12 @@ from os import path
 def test_emlfile(test_fs):
 
     eml=EmlFile(test_fs["mail1.eml"], 140, ia=False, force=False, ia_delay=2) # Updated constructor call
-    assert eml.system_timezone=="UTC"
+    assert eml.system_timezone=="CET"
     assert eml.file_encoding=="ascii"
-    assert eml.dt== datetime(2023, 9, 15, 7, 45, tzinfo=ZoneInfo(key='UTC')) 
+    assert eml.dt== datetime(2023, 9, 15, 9, 45, tzinfo=ZoneInfo(key='CET')) 
     assert eml.from_=="jane.smith@example.org"
     assert eml.subject=="Project Update EML Rename"
-    assert eml.final_name()=="20230915 0745 [jane.smith@example.org] Project Update EML Rename.eml"
+    assert eml.final_name()=="20230915 0945 [jane.smith@example.org] Project Update EML Rename.eml"
     assert eml.will_be_renamed(False)
     assert eml.filename_format_detected() is False
     assert path.exists(test_fs["mail1.eml"])
